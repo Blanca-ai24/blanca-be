1e11a24a01aefafb743f897d0be0cd84c6a8faf9
Mishael Dada <mishaeldada@gmail.com>
2024-02-23 16:52:47 +0100

    added chat conversation to strapi

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ChatConversations extends Schema.Component {
+  collectionName: 'components_chat_conversations';
+  info: {
+    displayName: 'Conversations';
+    icon: 'dashboard';
+  };
+  attributes: {
+    role: Attribute.String;
+    message: Attribute.String & Attribute.Required;
+    media: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'chat.conversations': ChatConversations;
+    }
+  }
 }
